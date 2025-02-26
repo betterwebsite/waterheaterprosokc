f7ac699bf73adaef92ca382aa702ad2602855302
Hmudda <normanwebdesigners@gmail.com>
2025-02-26 12:33:42 -0600

    initial

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,21 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Water Heater Pros",
+    "email": "waterheaterprosokc@gmail.com",
+    "phoneForTel": "405-496-3828",
+    "phoneFormatted": "(405) 496-3828",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
+        "lineOne": "",
+        "lineTwo": "",
+        "city": "Oklahoma City",
+        "state": "OK",
+        "zip": "73072",
         "country": "US",
         "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
     },
     "socials": {
-        "facebook": "https://www.facebook.com/",
+        "facebook": "https://www.facebook.com/p/Water-Heater-Pros-OKC-100064242121025",
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.legacy4plumbing.com"
 };
